b40c0c1cda5765f8e611533b7da786d710039a9e
Geewynxofficial <godwinning247@gmail.com>
2023-08-19 09:13:18 +0100

    table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- Create a table in the current DATABASE
 -- if table already exist, script should not fail
-CREATE TABLE first_table (
+CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
 );
