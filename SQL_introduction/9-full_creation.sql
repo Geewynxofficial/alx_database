b9839f6fbc4de77a841a297472514ee7e8f0e1e9
Geewynxofficial <godwinning247@gmail.com>
2023-08-19 10:07:19 +0100

    full project

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,1 +1,15 @@
+ -- create a table 
+ CREATE TABLE second_table (id INT, 
+ name VARCHAR(256)
+ score INT
+ );
+
+ -- Inserting data
+ INSERT INTO second_table VALUES 
+ (1, "John", 10),
+ (1, "Alex", 3),
+ (1, "Bob", 14),
+ (1, "George", 8);
+ 
+ 
  
