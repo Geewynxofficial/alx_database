7dbf4a60ffcb168826e33addf84a8d69a608e242
Geewynxofficial <godwinning247@gmail.com>
2023-08-19 10:34:31 +0100

    FILL

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,15 +1,15 @@
 -- create a table 
- CREATE TABLE IF NOT EXISTS second_table(id INT AUTO_INCREMENT PRIMARY KEY
- name VARCHAR(256)
+ CREATE TABLE IF NOT EXISTS second_table(id INT AUTO_INCREMENT PRIMARY KEY,
+ name VARCHAR(256),
  score INT
  );
 
 -- Inserting data
  INSERT INTO second_table(id, name, score) VALUES 
- (1, "John", 10),
- (2, "Alex", 3),
- (3, "Bob", 14),
- (4, "George", 8);
+ (1, 'John', 10),
+ (2, 'Alex', 3),
+ (3, 'Bob', 14),
+ (4, 'George', 8);
  
  
  
