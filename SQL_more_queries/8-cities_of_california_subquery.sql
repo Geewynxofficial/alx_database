7a48f7940b4c6f86d097a0c4c6ea65c312ef5b01
Geewynxofficial <godwinning247@gmail.com>
2023-08-23 19:11:02 +0100

    city

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- list of all cities in the database
 SELECT cities.id, cities.name 
 FROM cities
-ORDER BY cities.id ASC
-LIMIT 3;
+ORDER BY cities.id ASC;
+-- LIMIT 3;
 -- WHERE cities.state_id = (
 --   SELECT states.id FROM states
 --   WHERE states.name = 'California'
