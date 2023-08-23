d3fa70f45987fb9954d5a8f74468cedc54a9f3db
Geewynxofficial <godwinning247@gmail.com>
2023-08-23 01:01:22 +0100

    foreign

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,4 +1,11 @@
---create database and table
+-- create database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL
+);
+
+ALTER TABLE cities
+ADD FOREIGN KEY (state_id) REFERENCES states (id);
