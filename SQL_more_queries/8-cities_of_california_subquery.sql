a8e2320ec828f9b26f208a911c2a13a520fc99d9
Geewynxofficial <godwinning247@gmail.com>
2023-08-23 01:29:43 +0100

    ascend

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,6 @@
 -- list of all cities in the database
-SELECT cities.name FROM cities
+SELECT cities.name FROM cities;
+ORDER BY cities.id ASC;
 WHERE cities.state_id = (
   SELECT states.id FROM states
   WHERE states.name = 'California'
